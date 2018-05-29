30dcd458c51a2a48020bb04dad2c4797ec8792d9
parsy_a <lucas.parsy@epitech.eu>
2018-05-29 05:39:55 +0200

    completed transitive implem of ArticleViewer
    will be switched from AppCompatActivity to Fragment one of these days,
    it will be hard to refactor.
    
    added strings and colors.

--- app/src/main/java/fr/tikione/c2e/ArticleViewer.kt
+++ app/src/main/java/fr/tikione/c2e/ArticleViewer.kt
@@ -1,22 +1,15 @@
 package fr.tikione.c2e
 
+import android.app.Fragment
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.webkit.WebViewClient
-import android.view.Menu
 import kotlinx.android.synthetic.main.article_webview.*
-import android.view.WindowManager
 import android.content.SharedPreferences
 import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.support.design.widget.AppBarLayout
-import android.view.MenuItem
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import android.view.*
 import fr.tikione.c2e.Utils.TmpUtils
 import java.io.File
-import android.support.design.widget.CoordinatorLayout
-
-
 
 
 class ArticleViewer : AppCompatActivity() {
@@ -26,36 +19,48 @@ class ArticleViewer : AppCompatActivity() {
     private lateinit var preferences: SharedPreferences
     private lateinit var menu: Menu
 
+
+    /*
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle): View? {
+
+        val root =  inflater.inflate(R.layout.article_webview, container, false)
+        return root;
+    }
+*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val b = intent.extras
         preferences = this.getSharedPreferences("app", MODE_PRIVATE);
-/*
+
         val color = b.getInt("color")
-        val header = b.getString("header")
-        val url = b.getString("url")
+        val url = b.getString("file")
+        val title = b.getString("title")
+        val num = b.getInt("num")
         darkMode = preferences.getBoolean("darkMode", false)
         fontSize = preferences.getInt("fontSize", 14)
-*/
+
+
+
         setContentView(R.layout.article_webview)
         setSupportActionBar(toolbar);
-        collapsing_toolbar.setContentScrimColor(resources.getColor(android.R.color.holo_orange_light))
+        collapsing_toolbar.setContentScrimColor(color)
 
         val bar = supportActionBar
         bar?.setDisplayHomeAsUpEnabled(true)
         bar?.setDisplayShowHomeEnabled(true)
         bar?.setDisplayShowTitleEnabled(false)
 
-        val header = ""
-        val head = File(header)
+        val head = File(File(File(TmpUtils.getFilesPath(this.baseContext), num.toString()), "headers"), "$url.jpg")
         if (head.exists() && head.isFile && head.canRead()) {
-            val bit = BitmapFactory.decodeFile(header)
+            val bit = BitmapFactory.decodeFile(head.absolutePath)
             backgroundImageView.setImageBitmap(bit)
         } else {
             appBarLayout.layoutParams.height = convertDipToPixels(66.0f)
             //backgroundImageView.setBackgroundColor(color)
-            backgroundImageView.setBackgroundColor(resources.getColor(android.R.color.holo_orange_light))
+            backgroundImageView.setBackgroundColor(color)
         }
 
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS) //transparent status bar
@@ -65,7 +70,7 @@ class ArticleViewer : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
 
-        val file = File(TmpUtils.getFilesPath(this.baseContext), "test.html")
+        val file = File(File(TmpUtils.getFilesPath(this.baseContext), num.toString()), "$url.html")
         webView.loadUrl("file:///" + file.absolutePath)
     }
 
@@ -74,13 +79,21 @@ class ArticleViewer : AppCompatActivity() {
     }
 
     private fun setDarkMode() {
-        webView.evaluateJavascript("setDarkMode();", null)
+        webView.evaluateJavascript("switchTheme();", null)
         val item = menu.getItem(0)
         if (!darkMode)
             item.icon = resources.getDrawable(R.drawable.baseline_wb_sunny_black_18dp, null)
         else
             item.icon = resources.getDrawable(R.drawable.baseline_brightness_3_black_18dp, null)
     }
+
+/*
+    override fun onCreateOptionsMenu(nMenu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.article_menu, nMenu)
+        this.menu = nMenu!!
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+*/
 
     override fun onCreateOptionsMenu(nMenu: Menu): Boolean {
         menuInflater.inflate(R.menu.article_menu, nMenu)
