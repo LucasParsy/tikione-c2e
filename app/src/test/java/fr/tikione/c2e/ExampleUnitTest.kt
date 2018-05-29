94f6ea8a0b8885dfb25375234af437cf101b4e50
parsy_a <lucas.parsy@epitech.eu>
2018-05-29 05:27:41 +0200

    new unit tests
    for testing java features

--- app/src/test/java/fr/tikione/c2e/ExampleUnitTest.kt
+++ app/src/test/java/fr/tikione/c2e/ExampleUnitTest.kt
@@ -2,11 +2,13 @@ package fr.tikione.c2e
 
 import android.util.Log
 import compat.Tools
+import fr.tikione.c2e.core.SimpleMagArticle
 import org.junit.Test
 
 import org.junit.Assert.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -22,6 +24,34 @@ class ExampleUnitTest {
     @Test
     fun date_format() {
         assertEquals("1er déc 2018", DateCustom(2018, 12, true).dateToString())
+    }
+
+    @Test
+    fun string_split() {
+        var url = "background-image: url('/sites/default/files/numeros/381/5876-dices.jpeg');');"
+        val res = "/sites/default/files/numeros/381/5876-dices.jpeg"
+
+        url = url.substring(url.indexOf("'") + 1);
+        url = url.substring(0, url.indexOf("'"));
+
+        assertEquals(res, url)
+    }
+
+    @Test
+    fun hash_length() {
+        val title = "test"
+        val hash = title.hashCode().toString().replace('-', 'a')
+        println(hash)
+        assert(hash.length < 15)
+    }
+
+    @Test
+    fun data_classes() {
+        val data1 = SimpleMagArticle("titre")
+        val hash = data1.filename
+        val data2 = data1.copy()
+
+        assertEquals(hash, data2.filename)
     }
 
     @Test
